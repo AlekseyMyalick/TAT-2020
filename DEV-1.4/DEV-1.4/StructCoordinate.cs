28857c8e4f096eb70a69f728af40704f6aee6209
Alexey Myalick <myalick2017@yandex.ru>
2020-03-24 21:19:01 +0300

    Add classes: Bird, Airplane, Drone

---
+++ DEV-1.4/DEV-1.4/StructCoordinate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1._4
+{
+
+    public struct Coordinate
+    {
+        private float _x;
+        private float _y;
+        private float _z;
+
+        public Coordinate(float x, float y, float z)
+        {
+            if (x < 0 || y < 0 || z < 0)
+            {
+                throw new Exception("Less then zero");
+            }
+            this._x = x;
+            this._y = y;
+            this._z = z;
+        }
+    }
+}
+
