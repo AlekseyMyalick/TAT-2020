db94c21604a772f009194b1bc1e949b31ddbf7bb
Alexey Myalick <myalick2017@yandex.ru>
2020-03-26 21:05:27 +0300

    command line input

--- NumberSystemCalculator/NumberSystemCalculator/Program.cs
+++ NumberSystemCalculator/NumberSystemCalculator/Program.cs
@@ -8,8 +8,16 @@ namespace DEV_1_2
 {
     class Program
     {
+        public static int Number { get; set; }
+        public static int NumberSystem { get; set; }
+
         static void Main(string[] args)
         {
+                Number = Convert.ToInt32(args[0]);
+                NumberSystem = Convert.ToInt32(args[1]);
+                var numbersConverter = new NumberSystem(Number, NumberSystem);
+                var convertingResult = numbersConverter.TransferToNewSystem();
+                Console.WriteLine($"Result of converting = {convertingResult}"); 
         }
     }
 }
