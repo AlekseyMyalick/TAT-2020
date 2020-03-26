c1acc49464ead9ebcc8a5d43b4e54dff73149109
Alexey Myalick <myalick2017@yandex.ru>
2020-03-26 18:41:06 +0300

    added class and event

---
+++ TeacherStudent/TeacherStudent/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+            Student student1 = new Student("Hleb", "Kokorev");
+            student1.DoTask();
+            Student student2 = new Student("Aleksey", "Grushevich");
+            student2.DoTask();
+            Student student3 = new Student("ksenia", "Tarasevich");
+            student3.DoTask();
+            Student student4 = new Student("Aleksey", "Myalick");
+            student4.DoTask();
+            Student student5 = new Student("Anton", "Migel");
+            student5.DoTask();
+            Student student6 = new Student("kirill", "Babey");
+            student6.DoTask();
+            Student student7 = new Student("Viktoria", "Sivenkova");
+            student7.DoTask();
+            Student student8 = new Student("Aleksander", "Koralev");
+            student8.DoTask();
+            Student student9 = new Student("Kirill", "Skhlyrevsky");
+            student9.DoTask();
+            Student student10 = new Student("Pavel", "Lastovski");
+            student10.DoTask();
+
+            Student.
+        }
+    }
+}
---
+++ TeacherStudent/TeacherStudent/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherStudent
+{
+    public class Student: ITaskPerfomer
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public delegate string StudentTask();
+        public event StudentTask Notify;
+
+        public Student(string firstName, string lastName)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string DoTask()
+        {
+            Notify();
+            return GeneratesRandomString();
+        }
+
+
+        public string GeneratesRandomString() {
+            Random random = new Random();
+            int size = random.Next(1,15);
+            string answer = String.Empty;
+
+            for(int i = 0; i < size; i++)
+            {
+                int value = random.Next(32, 127);
+                answer += (char)value;
+            }
+
+            return answer;
+        }
+    }
+}
