3156caa7ed1f5021c0f95ee44972c55a7d5470df
Alexey Myalick <myalick2017@yandex.ru>
2020-04-29 19:29:54 +0300

    Add opening browser window

---
+++ Task-2.3/Task-2.3/FirstMail.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task_2._3
+{
+    class FirstMail
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpeningSearchEngine()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [Test]
+        public void 
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
